8415364d9a9189a60cd0ad47dc2b0ff51d565197
RichardLourie <32876317+RichardLourie@users.noreply.github.com>
2023-11-14 20:52:45 -0500

    Username setting and tracking work

--- HW4/UserContext.js
+++ HW4/UserContext.js
@@ -1,6 +1,13 @@
-// UserContext.js
-import React from 'react';
+import React, { createContext, useState } from 'react';
 
-const UserContext = React.createContext(null);
+export const UserContext = createContext();
 
-export default UserContext;
+export const UserProvider = ({ children }) => {
+  const [username, setUsername] = useState('');
+
+  return (
+    <UserContext.Provider value={{ username, setUsername }}>
+      {children}
+    </UserContext.Provider>
+  );
+};

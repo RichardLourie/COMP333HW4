df704b8e32be4ed0d4ea98e5e23896406f3d95d6
arieaton <144484549+arieaton@users.noreply.github.com>
2023-11-15 00:34:46 -0500

    add stats

--- HW4/components/mainpage.js
+++ HW4/components/mainpage.js
@@ -6,77 +6,113 @@ import { useApi } from '../APIContext.js'; // Import the useApi hook
 
 const MainPage = () => {
   const [ratings, setRatings] = useState([]);
+  const [statsData, setStatsData] = useState(null); // Added state for stats data
   const navigation = useNavigation();
   const { username } = useContext(UserContext);
   const { ipAddress } = useApi();
 
   useEffect(() => {
-    const fetchRatings = async () => {
+    const fetchData = async () => {
       try {
-        const response = await fetch(`http://${ipAddress}/index.php/song/list`);
-        const data = await response.json();
-        setRatings(data);
+        // Fetch ratings data
+        const responseRatings = await fetch(`http://${ipAddress}/index.php/song/list`);
+        const dataRatings = await responseRatings.json();
+        setRatings(dataRatings);
+
+        // Fetch stats data
+        const responseStats = await fetch(`http://${ipAddress}/index.php/song/stats`);
+        const dataStats = await responseStats.json();
+        setStatsData(dataStats);
       } catch (error) {
-        console.error('Error fetching ratings:', error);
+        console.error('Error fetching data:', error);
       }
     };
 
-    fetchRatings();
-  }, []);
+    fetchData();
+  }, [ipAddress]); // Include ipAddress as a dependency for useEffect
 
   const renderItem = ({ item }) => (
-  <View style={styles.item}>
-    <Text style={styles.cell}>{item.username}</Text>
-    <Text style={styles.cell}>{item.artist}</Text>
-    <Text style={styles.cell}>{item.song}</Text>
-    <Text style={styles.cell}>{item.rating}</Text>
-    {username === item.username && (
+    <View style={styles.item}>
+      <Text style={styles.cell}>{item.username}</Text>
+      <Text style={styles.cell}>{item.artist}</Text>
+      <Text style={styles.cell}>{item.song}</Text>
+      <Text style={styles.cell}>{item.rating}</Text>
+      {username === item.username && (
+        <Button
+          title="Delete"
+          onPress={() => navigation.navigate('deletepage', { itemId: item.id })}
+        />
+      )}
+      {username === item.username && (
+        <Button
+          title="Edit"
+          onPress={() => navigation.navigate('editpage', { itemId: item.id, itemArtist: item.artist, itemSong: item.song, itemRating: item.rating })}
+        />
+      )}
       <Button
-        title="Delete"
-        onPress={() => navigation.navigate('deletepage', { itemId: item.id })}
-      />  
-    )}
-    {username === item.username && (
-      <Button
-        title="Edit"
-        onPress={() => navigation.navigate('editpage', { itemId: item.id, itemArtist: item.artist, itemSong: item.song, itemRating: item.rating})}
-      />  
-    )}
-    <Button
         title="View"
-        onPress={() => navigation.navigate('viewpage', { itemId: item.id, itemArtist: item.artist, itemSong: item.song, itemRating: item.rating})}
-      />  
-  </View>
-);
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.username}>Welcome, {username}!</Text>
-      <Button
-        title="Add Rating"
-        onPress={() => navigation.navigate('addSong')}
-      />
-      <FlatList
-        data={ratings}
-        keyExtractor={item => item.id.toString()}
-        renderItem={renderItem}
-        ListHeaderComponent={
-          <View style={styles.item}>
-            <Text style={styles.cell}>Username</Text>
-            <Text style={styles.cell}>Artist</Text>
-            <Text style={styles.cell}>Song</Text>
-            <Text style={styles.cell}>Rating</Text>
-          </View>
-        }
+        onPress={() => navigation.navigate('viewpage', { itemId: item.id, itemArtist: item.artist, itemSong: item.song, itemRating: item.rating })}
       />
     </View>
   );
+
+  return (
+      <View style={styles.container}>
+        <Text style={styles.username}>Welcome, {username}!</Text>
+        <Button
+          title="Add Rating"
+          onPress={() => navigation.navigate('addSong')}
+        />
+        <FlatList
+          data={ratings}
+          keyExtractor={item => item.id.toString()}
+          renderItem={renderItem}
+          ListHeaderComponent={
+            <View style={styles.item}>
+              <Text style={styles.cell}>Username</Text>
+              <Text style={styles.cell}>Artist</Text>
+              <Text style={styles.cell}>Song</Text>
+              <Text style={styles.cell}>Rating</Text>
+            </View>
+          }
+        />
+        {statsData && (
+          <View style={styles.statsContainer}>
+            <Text style={styles.statsTitle}>Artist Statistics</Text>
+            <View style={styles.stats}>
+              <FlatList
+                data={statsData.artists}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                  <Text>
+                    Artist: {item.artist}, Entry Count: {item.entry_count}, Average Rating: {item.average_rating}
+                  </Text>
+                )}
+              />
+            </View>
+            <Text style={styles.statsTitle}>Song Statistics</Text>
+            <View style={styles.stats}>
+              <FlatList
+                data={statsData.songs}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                  <Text>
+                    Song: {item.song}, Entry Count: {item.entry_count}, Average Rating: {item.average_rating}
+                  </Text>
+                )}
+              />
+            </View>
+          </View>
+        )}
+      </View>
+    );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 20,
+    marginBottom: 20,
   },
   item: {
     flexDirection: 'row',
@@ -89,11 +125,24 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   username: {
-  fontSize: 18,
-  fontWeight: 'bold',
-  padding: 10,
-  textAlign: 'center',
-},
+    fontSize: 18,
+    fontWeight: 'bold',
+    padding: 10,
+    textAlign: 'center',
+  },
+  statsTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  statsContainer: {
+    alignItems: 'center',
+  },
+  stats: {
+    width: '80%', // Adjust the width as needed
+  },
 });
 
 export default MainPage;

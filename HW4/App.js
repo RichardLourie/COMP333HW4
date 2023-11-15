83e3c8157e5c85a19d6030f731aa76806af189ec
RichardLourie <32876317+RichardLourie@users.noreply.github.com>
2023-11-14 20:07:29 -0500

    Fixed username tracking start

--- HW4/App.js
+++ HW4/App.js
@@ -4,21 +4,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainPage from './components/mainpage.js';
 import addSongPage from './components/addSong.js';
-import UserProvider from './UserProvider';
+import UserProvider from './UserProvider.js'; 
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <UserProvider> {/* Wrap NavigationContainer with UserProvider */}
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Main" component={MainPage} />
-          <Stack.Screen name="AddSong" component={AddSongPage} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </UserProvider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Main" component={MainPage} />
+        <Stack.Screen name="addSong" component={addSongPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
